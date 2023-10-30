22d753e3853f9a199e9ae08d1e1610f47a589d04
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-10-30 17:26:39 +0300

    Added client-side validation for login and sign-up form

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,24 +1,58 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
-	const handleLogin = () => {
-		// handle login
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+	const [error, setError] = useState(null);
+
+	const handleInputChange = (e) => {
+		const { id, value } = e.target;
+		setFormData({ ...formData, [id]: value });
+
+		if (error) setError(null);
+	};
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		// query database and check for match, if not display eror
+		setError('No account found, email or password may be incorrect.');
 	};
 
 	return (
 		<>
 			<div className="login">
-				<div className="title">Login</div>
-				<form onSubmit={handleLogin}>
-					<input type="email" id="email" placeholder="Email" />
-					<input type="password" id="password" placeholder="Password" />
-					<button type="submit" id="submit">
-						Login
-					</button>
-				</form>
-				<p>
-					Don&apos;t have an account? <Link to="/SignUp">Sign up here</Link>
-				</p>
+				<div className="container">
+					<div className="title">Login</div>
+					<form onSubmit={handleLogin}>
+						<input
+							type="email"
+							id="email"
+							placeholder="Email"
+							value={formData.email}
+							onChange={handleInputChange}
+							required
+						/>
+						<input
+							type="password"
+							id="password"
+							placeholder="Password"
+							value={formData.password}
+							onChange={handleInputChange}
+							required
+						/>
+						{error && <p className="error">{error}</p>}
+						<button type="submit" id="submit">
+							Login
+						</button>
+					</form>
+					<p>
+						Don&apos;t have an account? <Link to="/SignUp">Sign up here</Link>
+					</p>
+				</div>
 			</div>
 		</>
 	);
--- frontend/src/components/pages/SignUp.jsx
+++ frontend/src/components/pages/SignUp.jsx
@@ -1,25 +1,98 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-	const handleSignUp = () => {
-		// handle login
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+	const [error, setError] = useState({
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+
+	const handleInputChange = (e) => {
+		const { id, value } = e.target;
+		setFormData({ ...formData, [id]: value });
+
+		setError({ ...error, [id]: '' });
+	};
+
+	const handleSignUp = (e) => {
+		e.preventDefault();
+
+		const customError = {
+			email: '',
+			password: '',
+			confirmPassword: '',
+		};
+
+		// Password validation
+		if (formData.password.length < 6) {
+			customError.password = 'Password must be at least 6 characters long.';
+		}
+		if (!/[^a-zA-Z0-9]/.test(formData.password)) {
+			customError.password = 'Password must contain at least 1 special character.';
+		}
+
+		// Password confirmation
+		if (formData.password !== formData.confirmPassword) {
+			customError.confirmPassword = 'Passwords do not match.';
+		}
+
+		setError(customError);
+
+		// If there are errors, do not proceed with the signup
+		if (Object.values(customError).some((message) => message !== '')) {
+			return;
+		}
+
+		// send data to server here
 	};
 
 	return (
 		<>
 			<div className="sign-up">
-				<div className="title">Sign Up</div>
-				<form onSubmit={handleSignUp}>
-					<input type="email" id="email" placeholder="Email" />
-					<input type="password" id="password" placeholder="Password" />
-					<input type="password" id="confirm-password" placeholder="Confirm Password" />
-					<button type="submit" id="submit">
-						Sign Up
-					</button>
-				</form>
-				<p>
-					Already have an account? <Link to="/Login">Log in here</Link>
-				</p>
+				<div className="container">
+					<div className="title">Sign Up</div>
+					<form onSubmit={handleSignUp}>
+						<input
+							type="email"
+							id="email"
+							placeholder="Email"
+							value={formData.email}
+							onChange={handleInputChange}
+							required
+						/>
+						<input
+							type="password"
+							id="password"
+							placeholder="Password"
+							value={formData.password}
+							onChange={handleInputChange}
+							required
+						/>
+						<input
+							type="password"
+							id="confirmPassword"
+							placeholder="Confirm Password"
+							value={formData.confirmPassword}
+							onChange={handleInputChange}
+							required
+						/>
+						{error.email && <p className="error">{error.email}</p>}
+						{error.password && <p className="error">{error.password}</p>}
+						{error.confirmPassword && <p className="error">{error.confirmPassword}</p>}
+						<button type="submit" id="submit">
+							Sign Up
+						</button>
+					</form>
+					<p>
+						Already have an account? <Link to="/Login">Log in here</Link>
+					</p>
+				</div>
 			</div>
 		</>
 	);
