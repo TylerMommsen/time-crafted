fb8b6e97327bb2256476910878f9045d1837ef76
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2024-01-31 22:35:34 +0300

    Added media queries for header and navbar to also create burger menu

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -7,6 +7,12 @@ const Header = ({ currPage }) => {
 	const [scrolled, setScrolled] = useState(false);
 	const [isHidden, setIsHidden] = useState(false);
 	const [prevScrollPos, setPrevScrollPos] = useState(0);
+	const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1200);
+	const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+	const toggleBurgerMenu = () => {
+		setIsBurgerMenuOpen((open) => !open);
+	};
 
 	useEffect(() => {
 		if (currPage !== 'home') {
@@ -40,27 +46,76 @@ const Header = ({ currPage }) => {
 		// eslint-disable-next-line
 	}, [prevScrollPos, currPage]);
 
+	useEffect(() => {
+		const handleResize = () => {
+			setIsSmallScreen(window.innerWidth < 1200);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	const closeBurgerMenu = () => {
+		setIsBurgerMenuOpen(false);
+	};
+
 	return (
 		<>
 			<header className={`${scrolled ? 'scrolled' : ''} ${isHidden ? 'hidden' : ''}`}>
-				<h1 className="header-title">
-					<Link to="/home">Time Crafted</Link>
-				</h1>
-				<NavBar />
-				<div className="header-cart-selections">
-					<Link
-						to="/wishlist"
-						onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
-					>
-						<img id="wishlist-icon" src="/wishlist-icon.png"></img>
-					</Link>
-					<Link
-						to="/login"
-						onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
-					>
-						<img id="user-icon" src="/user-icon.png"></img>
-					</Link>
-				</div>
+				{isSmallScreen ? (
+					<>
+						<div className="burger-nav">
+							<img
+								src="/burger-menu.svg"
+								className={`burger-menu-icon ${isBurgerMenuOpen ? 'open' : ''}`}
+								onClick={toggleBurgerMenu}
+							></img>
+							{isBurgerMenuOpen && <NavBar closeBurgerMenu={closeBurgerMenu} />}
+						</div>
+
+						<h1 className="header-title">
+							<Link to="/home">Time Crafted</Link>
+						</h1>
+						<div className="header-cart-selections">
+							<Link
+								to="/wishlist"
+								onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
+							>
+								<img id="wishlist-icon" src="/wishlist-icon.png"></img>
+							</Link>
+							<Link
+								to="/login"
+								onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
+							>
+								<img id="user-icon" src="/user-icon.png"></img>
+							</Link>
+						</div>
+					</>
+				) : (
+					<>
+						<h1 className="header-title">
+							<Link to="/home">Time Crafted</Link>
+						</h1>
+						<NavBar />
+						<div className="header-cart-selections">
+							<Link
+								to="/wishlist"
+								onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
+							>
+								<img id="wishlist-icon" src="/wishlist-icon.png"></img>
+							</Link>
+							<Link
+								to="/login"
+								onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}
+							>
+								<img id="user-icon" src="/user-icon.png"></img>
+							</Link>
+						</div>
+					</>
+				)}
 			</header>
 		</>
 	);
--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -1,38 +1,87 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const NavBar = () => {
-	const scrollToTop = () => {
+const NavBar = ({ closeBurgerMenu }) => {
+	const [isBurgerMenu, setIsBurgerMenu] = useState(window.innerWidth < 1200);
+	const handleClick = () => {
 		window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+
+		if (isBurgerMenu) closeBurgerMenu();
 	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsBurgerMenu(window.innerWidth < 1200);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
 	return (
 		<>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/home" onClick={scrollToTop}>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link to="/shop" onClick={scrollToTop}>
-							Shop
-						</Link>
-					</li>
-					<li>
-						<Link to="/about" onClick={scrollToTop}>
-							About
-						</Link>
-					</li>
-					<li>
-						<Link to="/contact" onClick={scrollToTop}>
-							Contact
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			{isBurgerMenu ? (
+				<nav>
+					<img src="/exit-icon.png" className="exit-icon" onClick={handleClick}></img>
+					<ul>
+						<li>
+							<Link to="/home" onClick={handleClick}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to="/shop" onClick={handleClick}>
+								Shop
+							</Link>
+						</li>
+						<li>
+							<Link to="/about" onClick={handleClick}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to="/contact" onClick={handleClick}>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			) : (
+				<nav>
+					<ul>
+						<li>
+							<Link to="/home" onClick={handleClick}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to="/shop" onClick={handleClick}>
+								Shop
+							</Link>
+						</li>
+						<li>
+							<Link to="/about" onClick={handleClick}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to="/contact" onClick={handleClick}>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			)}
 		</>
 	);
+};
+
+NavBar.propTypes = {
+	closeBurgerMenu: PropTypes.func,
 };
 
 export default NavBar;
